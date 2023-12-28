5cd01e0652d14a48038abff27007cbb1f87c1e7b
Ekaterina <klarina2409@gmail.com>
2023-12-29 01:12:35 +0300

    мастер

---
+++ exam-app/src/test/java/ru/skypro/exam/ExamAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
